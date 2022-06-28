4321bb6890db2c6a849583a641cf08cdf9c155ba
Sivadommeti09 <101792428+Sivadommeti09@users.noreply.github.com>
2022-06-28 14:14:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "ap-south-1"
-    access_key = "AKIA5L3RRIIRHURKOHVT"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
